13b44df3d14df2cf47f34ac67d1414e17b040443
PiyushPanwarFST <piyush2002panwar@gmail.com>
2025-11-19 10:36:18 +0530

    updated version

--- Easy/Array_subset.py
+++ Easy/Array_subset.py
@@ -1,3 +1,18 @@
+# Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+
+# Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+# Output: true
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [1, 2, 3, 4, 4, 5, 6], b[] = [1, 2, 4]
+# Output: true
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [10, 5, 2, 23, 19], b[] = [19, 5, 3]
+# Output: false
+# Explanation: b[] is not a subset of a[]
+# Constraints:
+# 1 <= a.size(), b.size() <= 105
+# 1 <= a[i], b[j] <= 106
+
 class Solution:
     # Two - pointer approach
     def isSubset(self, a, b):
@@ -33,7 +48,6 @@ class Solution:
         
     # Hashing approach without library (Manual) 
     def isSubset(self, a, b):
-        
         map = {}
         for num in a:
             if num in map:
@@ -46,3 +60,9 @@ class Solution:
                 return False
             map[num] -= 1
         return True
+    
+if __name__ == "__main__":
+    a = [11, 1, 13, 21, 3, 7]
+    b = [11, 3, 7, 1]
+    obj = Solution()
+    print(obj.isSubset(a, b))
--- Easy/Square_root.py
+++ Easy/Square_root.py
@@ -1,17 +1,17 @@
-Given a positive integer n, find the square root of n. If n is not a perfect square, then return the floor value.
-Floor value of any number is the greatest Integer which is less than or equal to that number.
+# Given a positive integer n, find the square root of n. If n is not a perfect square, then return the floor value.
+# Floor value of any number is the greatest Integer which is less than or equal to that number.
 
-Input: n = 4
-Output: 2
-Explanation: Since, 4 is a perfect square, so its square root is 2.
-Input: n = 11
-Output: 3
-Explanation: Since, 11 is not a perfect square, floor of square root of 11 is 3.
-Input: n = 1
-Output: 1
-Explanation: 1 is a perfect sqaure, so its square root is 1.
-Constraints:
-1 ≤ n ≤ 3*104
+# Input: n = 4
+# Output: 2
+# Explanation: Since, 4 is a perfect square, so its square root is 2.
+# Input: n = 11
+# Output: 3
+# Explanation: Since, 11 is not a perfect square, floor of square root of 11 is 3.
+# Input: n = 1
+# Output: 1
+# Explanation: 1 is a perfect sqaure, so its square root is 1.
+# Constraints:
+# 1 ≤ n ≤ 3*104
 
 class Solution:
     def floorSqrt(self, n): 
@@ -28,5 +28,5 @@ class Solution:
                 
             else:
                 high = mid - 1
-            
+
         return res
